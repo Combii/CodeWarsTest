dc4b3a0981aec1368406f481ee555cc84026be2d
Combii <davidstovle@gmail.com>
2016-11-02 15:56:25 +0100

    First Upload

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+/**
+ * Created by David Stovlbaek on 13/10/16.
+ */
+public class Person {
+    public int age;
+    public String name;
+    public String gender;
+
+    public Person(){
+        this(null, 0, null);
+    }
+
+    public Person(int age) {
+        this(null, age, null);
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        if(gender != null) {
+                this.gender = gender;
+        }
+        else
+            this.gender = "Male";
+    }
+}
---
+++ src/RunProgram.java
@@ -0,0 +1,43 @@
+
+import java.util.Arrays;
+
+/**
+ * Created by David Stovlbaek on 17/09/16.
+ */
+public class RunProgram {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
+
+
+
+
+
+    /*
+    private static String RANK = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String nthRank(String st, Integer[] we, int n) {
+        String[] ary = st.split(",");
+        for(String i: ary) {
+            System.out.println(i);
+            System.out.println(getSum(i));
+        }
+            return "";
+    }
+
+    private static int getSum(String st){
+        int sum = 0;
+        for(int i = 0; i < st.length(); i++)
+            sum += RANK.indexOf(st.toLowerCase().charAt(i) + 1);
+        return sum + st.length();
+    }
+
+}
+
+*/
